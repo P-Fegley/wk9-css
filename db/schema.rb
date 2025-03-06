1ba8fa0e162ac3eafc4eb4af96ae03720d6082b8
Preston Fegley <fegley.preston@gmail.com>
2025-03-06 05:31:27 +0000

    end of class - a few file creations mostly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_02_28_013050) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_06_025056) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "body"
     t.string "image"
@@ -28,4 +37,6 @@ ActiveRecord::Schema[7.1].define(version: 2022_02_28_013050) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
 end
